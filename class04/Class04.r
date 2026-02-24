0bf12774873d6b4d7644975cd0e5ae08e27f013c
Erin McTavish <emctavish@ucsd.edu>
2026-02-24 12:25:00 -0800

    uploading classes

---
+++ class04/Class04.r
@@ -0,0 +1,11 @@
+#' ---
+#' title: "Class 4 R Intro"
+#' author: "Erin McTavish"
+#' date: "January 15, 2026"
+#' ---
+
+x <- 1:50
+plot(x, sin(x))
+
+plot(x,sin(x),typ="l", col="purple", lwd=2)
+log(10, base =10)
